11d6e2642fd4099afb35d0a3087a8bc9065b39d7
raphaelpg <raphael_pinto@outlook.com>
2020-03-30 16:13:16 +0200

    test internal functions

--- contracts/ERC20Example.sol
+++ contracts/ERC20Example.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.12;
+pragma solidity ^0.5.12;
 
 import "./ERC20.sol";
 import "./Ownable.sol";
@@ -14,5 +14,26 @@ contract ERC20Example is ERC20, Ownable {
         decimals = _decimals;
 
         _mint(msg.sender, totalSupply);
+    }
+
+    //Functions for testing purposes
+    function internalTransfer(address sender, address recipient, uint256 amount) public {
+    	_transfer(sender, recipient, amount);
+    }
+
+    function internalMint(address account, uint256 amount) public {
+    	_mint(account, amount);
+    }
+
+    function internalBurn(address account, uint256 amount) public {
+    	_burn(account, amount);
+    }
+
+    function internalApprove(address owner, address spender, uint256 amount) public {
+    	_approve(owner, spender, amount);
+    }
+
+    function internalBurnFrom(address account, uint256 amount) public {
+    	_burnFrom(account, amount);
     }
 }
--- test/TestERC20Example.js
+++ test/TestERC20Example.js
@@ -5,6 +5,7 @@ const ERC20Example = artifacts.require("ERC20Example");
 contract("ERC20Example", function(accounts){
     const owner = accounts[0];
     const spender = accounts[1];
+    const address0 = '0x0000000000000000000000000000000000000000';
     const _name = "MyTokenExample";
     const _symbol = "MTE";
     const _decimals = 18;
@@ -15,9 +16,11 @@ contract("ERC20Example", function(accounts){
         this.ERC20ExampleInstance = await ERC20Example.new(_name, _symbol, _decimals, _totalSupply);
     });
 
-    //Testing ERC20 fonctions
+
+    //Testing ERC20 metadata
     //Test 1
     it("Check token name", async function() {
+        console.log("Testing ERC20 metadata:");
         expect(await this.ERC20ExampleInstance.name.call()).to.equal("MyTokenExample");
     });
 
@@ -31,8 +34,11 @@ contract("ERC20Example", function(accounts){
         expect(await this.ERC20ExampleInstance.decimals.call()).to.be.bignumber.equal(new BN(18));
     });
 
+
+    //Testing ERC20 basic functions
     //Test 4
     it('Check totalSupply() function', async function () {
+        console.log("Testing ERC20 basic functions:");
         let totalSupply = await this.ERC20ExampleInstance.totalSupply();
         expect(totalSupply).to.be.bignumber.equal(new BN(1000000));
     });
@@ -56,6 +62,11 @@ contract("ERC20Example", function(accounts){
 
         expect(ownerBalanceAfter).to.be.bignumber.equal(ownerBalanceBefore.sub(amount));
         expect(spenderBalanceAfter).to.be.bignumber.equal(spenderBalanceBefore.add(amount));
+
+        //`recipient` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.transfer(address0, amount, {from: owner}),"ERC20: transfer to the zero address");        
+        //the caller must have a balance of at least `amount`
+        await expectRevert(this.ERC20ExampleInstance.transfer(owner, new BN('11'), {from: spender}),"ERC20: transfer amount exceeds balance");        
     });
 
     //Test 7
@@ -64,6 +75,9 @@ contract("ERC20Example", function(accounts){
         await this.ERC20ExampleInstance.approve(spender, amount, {from: owner});
         let amountApproved = await this.ERC20ExampleInstance.allowance(owner, spender); 
         expect(amountApproved).to.be.bignumber.equal(amount);
+
+        //`spender` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.approve(address0, amount, {from: owner}),"ERC20: approve to the zero address");        
     });
 
     //Test 8
@@ -81,6 +95,18 @@ contract("ERC20Example", function(accounts){
 
         expect(ownerBalanceAfter).to.be.bignumber.equal(ownerBalanceBefore.sub(amount));
         expect(spenderBalanceAfter).to.be.bignumber.equal(spenderBalanceBefore.add(amount));
+
+        //Testing reverts
+        //`sender` and `recipient` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.transferFrom(owner, address0, amount, {from: spender}),"Insufficient allowance");
+        await expectRevert(this.ERC20ExampleInstance.transferFrom(address0, spender, amount, {from: spender}),"Insufficient allowance");
+
+        //`sender` must have a balance of at least `amount`.
+        await this.ERC20ExampleInstance.approve(owner, new BN(20), {from: spender});
+        await expectRevert(this.ERC20ExampleInstance.transferFrom(spender, owner, new BN(20), {from: owner}),"ERC20: transfer amount exceeds balance");
+
+        //the caller must have allowance for `sender`'s tokens of at least `amount`.
+        await expectRevert(this.ERC20ExampleInstance.transferFrom(owner, spender, new BN(11), {from: spender}),"Insufficient allowance");
     });
 
     //Test 9
@@ -91,6 +117,9 @@ contract("ERC20Example", function(accounts){
         await this.ERC20ExampleInstance.increaseAllowance(spender, addedAmount, {from: owner});
         let amountApproved = await this.ERC20ExampleInstance.allowance(owner, spender); 
         expect(amountApproved).to.be.bignumber.equal(initialAmount.add(addedAmount));
+
+        //`spender` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.increaseAllowance(address0, addedAmount, {from: owner}),"ERC20: approve to the zero address");
     });
 
     //Test 10
@@ -101,36 +130,148 @@ contract("ERC20Example", function(accounts){
         await this.ERC20ExampleInstance.decreaseAllowance(spender, subtractedAmount, {from: owner});
         let amountApproved = await this.ERC20ExampleInstance.allowance(owner, spender); 
         expect(amountApproved).to.be.bignumber.equal(initialAmount.sub(subtractedAmount));
+
+        //Testing reverts
+        //`spender` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.decreaseAllowance(address0, subtractedAmount, {from: owner}),"ERC20: decreased allowance below zero");  
+
+        //`spender` must have allowance for the caller of at least `subtractedValue`.
+        let subtractedAmount2 = new BN('50');
+        await expectRevert(this.ERC20ExampleInstance.decreaseAllowance(spender, subtractedAmount2, {from: owner}),"ERC20: decreased allowance below zero");
+    });
+
+
+    //Testing ERC20 internal fonctions
+    //Test 11
+    it("Check _transfer() function", async function () {
+        console.log("Testing ERC20 internal functions:");
+        let ownerBalanceBefore = await this.ERC20ExampleInstance.balanceOf(owner);
+        let spenderBalanceBefore = await this.ERC20ExampleInstance.balanceOf(spender);
+        let amount = new BN('10');
+
+        await this.ERC20ExampleInstance.internalTransfer(owner, spender, amount, {from: owner});
+
+        let ownerBalanceAfter = await this.ERC20ExampleInstance.balanceOf(owner);
+        let spenderBalanceAfter = await this.ERC20ExampleInstance.balanceOf(spender);
+
+        expect(ownerBalanceAfter).to.be.bignumber.equal(ownerBalanceBefore.sub(amount));
+        expect(spenderBalanceAfter).to.be.bignumber.equal(spenderBalanceBefore.add(amount));
+
+        //`sender` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.internalTransfer(address0, spender, amount),"ERC20: transfer from the zero address")
+
+        //`recipient` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.internalTransfer(owner, address0, amount, {from:owner}),"ERC20: transfer to the zero address")
+        
+        //`sender` must have a balance of at least `amount`.
+        await expectRevert(this.ERC20ExampleInstance.internalTransfer(owner, spender, new BN('1000001'), {from:owner}),"ERC20: transfer amount exceeds balance")
+    });
+
+    //Test 12
+    it("Check _mint() function", async function () {
+        let spenderBalanceBefore = await this.ERC20ExampleInstance.balanceOf(spender);
+        let amount = new BN('10');
+        let totalSupplyBefore = await this.ERC20ExampleInstance.totalSupply();
+
+        await this.ERC20ExampleInstance.internalMint(spender, amount);
+
+        let spenderBalanceAfter = await this.ERC20ExampleInstance.balanceOf(spender);
+        let totalSupplyAfter = await this.ERC20ExampleInstance.totalSupply();
+
+        expect(spenderBalanceAfter).to.be.bignumber.equal(spenderBalanceBefore.add(amount));
+        expect(totalSupplyAfter).to.be.bignumber.equal(totalSupplyBefore.add(amount));
+
+        //`recipient` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.internalMint(address0, amount),"ERC20: mint to the zero address")
+    });
+
+    //Test 13
+    it("Check _burn() function", async function () {
+        let ownerBalanceBefore = await this.ERC20ExampleInstance.balanceOf(owner);
+        let amount = new BN('10');
+        let totalSupplyBefore = await this.ERC20ExampleInstance.totalSupply();
+
+        await this.ERC20ExampleInstance.internalBurn(owner, amount);
+
+        let ownerBalanceAfter = await this.ERC20ExampleInstance.balanceOf(owner);
+        let totalSupplyAfter = await this.ERC20ExampleInstance.totalSupply();
+
+        expect(ownerBalanceAfter).to.be.bignumber.equal(ownerBalanceBefore.sub(amount));
+        expect(totalSupplyAfter).to.be.bignumber.equal(totalSupplyBefore.sub(amount));
+
+        //`account` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.internalBurn(address0, amount),"ERC20: burn from the zero address");
+
+        //`account` must have at least `amount` tokens.
+        await expectRevert(this.ERC20ExampleInstance.internalBurn(owner, new BN('1000000')),"ERC20: burn amount exceeds balance");
+    });
+
+    //Test 14
+    it("Check _approve() function", async function () {
+        let amount = new BN('10');
+        await this.ERC20ExampleInstance.internalApprove(owner, spender, amount, {from: owner});
+        let amountApproved = await this.ERC20ExampleInstance.allowance(owner, spender); 
+        expect(amountApproved).to.be.bignumber.equal(amount);
+
+        //`owner` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.internalApprove(address0, spender, amount),"ERC20: approve from the zero address");
+
+        //`spender` cannot be the zero address.
+        await expectRevert(this.ERC20ExampleInstance.internalApprove(owner, address0, amount, {from: owner}),"ERC20: approve to the zero address");        
+    });
+
+    //Test 15
+    it("Check _burnFrom() function", async function () {
+        let ownerBalanceBefore = await this.ERC20ExampleInstance.balanceOf(owner);
+        let amount = new BN('10');
+        let totalSupplyBefore = await this.ERC20ExampleInstance.totalSupply();
+
+        await this.ERC20ExampleInstance.approve(spender, amount, {from:owner});
+        let spenderAllowanceBefore = await this.ERC20ExampleInstance.allowance(owner, spender);
+
+        await this.ERC20ExampleInstance.internalBurnFrom(owner, amount, {from:spender});
+
+        let ownerBalanceAfter = await this.ERC20ExampleInstance.balanceOf(owner);
+        let totalSupplyAfter = await this.ERC20ExampleInstance.totalSupply();
+        let spenderAllowanceAfter = await this.ERC20ExampleInstance.allowance(owner, spender);
+
+        expect(ownerBalanceAfter).to.be.bignumber.equal(ownerBalanceBefore.sub(amount));
+        expect(totalSupplyAfter).to.be.bignumber.equal(totalSupplyBefore.sub(amount));
+        expect(spenderAllowanceAfter).to.be.bignumber.equal(spenderAllowanceBefore.sub(amount));
+
+        //`account` must have at least `amount` tokens.
+        await expectRevert(this.ERC20ExampleInstance.internalBurnFrom(owner, new BN('1000000')),"ERC20: burn amount exceeds balance");
     });
 
     //Testing Ownable fonctions
-    //Test 11
+    //Test 16
     it('Check owner() function', async function () {
+        console.log("Testing Ownable fonctions:");
         let currentOwner = await this.ERC20ExampleInstance.owner();
         expect(currentOwner).to.be.equal(owner);
     });
 
-    //Test 12
+    //Test 17
     it('Check isOwner() function', async function () {
         let isOwner = await this.ERC20ExampleInstance.isOwner({from: owner});
         expect(isOwner).to.be.equal(true);
     });
 
-    //Test 13
+    //Test 18
     it('Check renounceOwnership() function', async function () {
         await this.ERC20ExampleInstance.renounceOwnership({from: owner});
         let isOwner = await this.ERC20ExampleInstance.isOwner({from: owner});
         expect(isOwner).to.be.equal(false);
     })
 
-    //Test 14
+    //Test 19
     it('Check transferOwnership() function', async function () {
         await this.ERC20ExampleInstance.transferOwnership(spender, {from: owner});
         let isOwner = await this.ERC20ExampleInstance.isOwner({from: spender});
         expect(isOwner).to.be.equal(true);
     })
 
-    //Test 15
+    //Test 20
     it('Check onlyOwner() modifier', async function () {
         await expectRevert(this.ERC20ExampleInstance.transferOwnership(spender, {from: spender}),"Ownable: caller is not the owner");
     })    
